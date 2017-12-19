1175eef31b4ec2d09c3f6b4848cdf510a97297b5
Jonathan Daniel <jonathandaniel@fernbanklinks.com>
2017-12-19 18:36:00 -0500

    Initial Commit

---
+++ app/src/main/java/org/toastertech/toasterscout/activities/HomeScreen.java
@@ -0,0 +1,14 @@
+package org.toastertech.toasterscout.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import org.toastertech.toasterscout.R;
+
+public class HomeScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_screen);
+    }
+}
---
+++ app/src/main/java/org/toastertech/toasterscout/activities/ScoutingActivity.java
@@ -0,0 +1,14 @@
+package org.toastertech.toasterscout.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import org.toastertech.toasterscout.R;
+
+public class ScoutingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scouting);
+    }
+}
