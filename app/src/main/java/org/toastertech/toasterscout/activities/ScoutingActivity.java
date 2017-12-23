b588bfd29a36af002e14f84b835fb3f0d9a68167
Jonathan Daniel <gcagrad19@gmail.com>
2017-12-22 23:19:47 -0500

    Created Match Details Fragment
    This fragment will display the team number, match number and scout name for the current match.

--- app/src/main/java/org/toastertech/toasterscout/activities/ScoutingActivity.java
+++ app/src/main/java/org/toastertech/toasterscout/activities/ScoutingActivity.java
@@ -1,16 +1,21 @@
 package org.toastertech.toasterscout.activities;
 
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import org.toastertech.toasterscout.R;
 import org.toastertech.toasterscout.data.FileManager;
 import org.toastertech.toasterscout.data.Match;
+import org.toastertech.toasterscout.fragments.MatchDetailsFragment;
 
 
 public class ScoutingActivity extends AppCompatActivity {
     int currentMatchIndex;
 
+    MatchDetailsFragment matchDetailsFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,15 +35,16 @@ public class ScoutingActivity extends AppCompatActivity {
             currentMatchIndex = 0;
         }
 
-        reconfigureWidgets();
+        setupXMLVariables();
 
     }
+
 
     private void setupXMLVariables(){
-
+        matchDetailsFragment = (MatchDetailsFragment)getSupportFragmentManager().findFragmentById(R.id.matchDetailsFragment);
+        matchDetailsFragment.updateFragment(FileManager.currentMatches.get(currentMatchIndex));
     }
 
-    private void reconfigureWidgets(){
 
-    }
+
 }
---
+++ app/src/main/java/org/toastertech/toasterscout/fragments/MatchDetailsFragment.java
@@ -0,0 +1,50 @@
+package org.toastertech.toasterscout.fragments;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import android.widget.EditText;
+import org.toastertech.toasterscout.R;
+import org.toastertech.toasterscout.data.Match;
+
+
+public class MatchDetailsFragment extends Fragment {
+    View inflatedView;
+    EditText matchNumField, teamNumField, scoutNameField;
+
+
+    public MatchDetailsFragment() {
+        super();
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        System.out.println("onCreateView executed");
+
+        inflatedView = inflater.inflate(R.layout.fragment_match_details, container, false);
+        matchNumField  = (EditText) inflatedView.findViewById(R.id.matchNumTextField);
+        teamNumField   = (EditText) inflatedView.findViewById(R.id.teamNumTextField);
+        scoutNameField = (EditText) inflatedView.findViewById(R.id.scoutNameTextField);
+
+        return inflatedView;
+    }
+
+    public void updateFragment(Match match){
+        matchNumField .setText(String.valueOf(match.getMatchNum()));
+        teamNumField  .setText(String.valueOf(match.getTeamNum()));
+        scoutNameField.setText(match.getScoutName());
+    }
+
+    public String[] getValues(){
+        return new String[]{matchNumField.getText().toString(), teamNumField.getText().toString(), scoutNameField.getText().toString()};
+    }
+}
